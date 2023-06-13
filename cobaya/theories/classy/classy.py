541c797e0f602f9bcfeae9b1f63716b200d034d2
em632080 <em632080@login18-3.hpc.itc.rwth-aachen.de>
2023-06-13 12:26:19 +0200

    Test

--- cobaya/theories/classy/classy.py
+++ cobaya/theories/classy/classy.py
@@ -473,13 +473,19 @@ class classy(BoltzmannBase):
         self.extra_args["P_k_max_1/Mpc"] = max(k_max, k_max_old)
 
     def set(self, params_values_dict):
-        # If no output requested, remove arguments that produce an error
-        # (e.g. complaints if halofit requested but no Cl's computed.) ?????
-        # Needed for facilitating post-processing
 
+        #neuer Parameter
+        var=params_values_dict["asdf"]
+        
+        #leeres Dokument erzeugen
+        temp=[0,0]
+        np.savetxt("/home/em632080/software/cobayafork/test2/parameters.txt",temp)
+        
         #Parameter in txt datei schreiben
-        print("Es klappt")
-         
+        test=np.array(np.genfromtxt("/home/em632080/software/cobayafork/test2/parameters.txt"))
+        test=list(test[2:])
+        test.append(var)
+        np.savetxt("/home/em632080/software/cobayafork/test2/parameters.txt",test)
          
          
         #entfernen der Parameter
